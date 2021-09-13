7e0a47f8cf83dcbb4282829464196b01ab72543f
iambrunoromano <brunoromano@hotmail.it>
2021-09-13 10:26:17 +0200

    Fix for db coherence

--- MusicStore/src/main/java/com/musicstore/model/OrderBean.java
+++ MusicStore/src/main/java/com/musicstore/model/OrderBean.java
@@ -6,37 +6,49 @@ import java.util.Objects;
 public class OrderBean {
 
 	private int id;
+	private String mail;
 	private Timestamp date;
 	private double total;
-	private String mail;
+	
+	public OrderBean() {}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public Timestamp getDate() {
-		return date;
-	}
-	public void setDate(Timestamp date) {
-		this.date = date;
-	}
-	public double getTotal() {
-		return total;
-	}
-	public void setTotal(double total) {
-		this.total = total;
-	}
+
 	public String getMail() {
 		return mail;
 	}
+
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
+
+	public Timestamp getDate() {
+		return date;
+	}
+
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(date, id, mail, total);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -49,9 +61,10 @@ public class OrderBean {
 		return Objects.equals(date, other.date) && id == other.id && Objects.equals(mail, other.mail)
 				&& Double.doubleToLongBits(total) == Double.doubleToLongBits(other.total);
 	}
+
 	@Override
 	public String toString() {
-		return "OrderBean [id=" + id + ", date=" + date + ", total=" + total + ", mail=" + mail + "]";
+		return "OrderBean [id=" + id + ", mail=" + mail + ", date=" + date + ", total=" + total + "]";
 	}
-	
+		
 }
