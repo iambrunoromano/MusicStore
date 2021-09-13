49866dc206b909675acc57a4452fae2708682ad9
iambrunoromano <brunoromano@hotmail.it>
2021-09-13 12:08:25 +0200

    IShipmentRepository and ShipmentBean adaptation

--- MusicStore/src/main/java/com/musicstore/model/ShipmentBean.java
+++ MusicStore/src/main/java/com/musicstore/model/ShipmentBean.java
@@ -3,9 +3,18 @@ package com.musicstore.model;
 import java.sql.Timestamp;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ShipmentBean {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private Timestamp shipDate;
 	private Timestamp arriveDate;
 	private String shipAddress;
