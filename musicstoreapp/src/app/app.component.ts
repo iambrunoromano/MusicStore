0028e3ac257c891d6eb62c5743369f5b35ffb80d
iambrunoromano <brunoromano@hotmail.it>
2021-09-21 15:16:45 +0200

    Single category working app component

--- musicstoreapp/src/app/app.component.ts
+++ musicstoreapp/src/app/app.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Category } from './interfaces/category';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { CategoryService } from './services/category.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'musicstoreapp';
   root_url = 'http://localhost:8080/musicstore/api/';
-  customers:any;
-  constructor(private http: HttpClient){}
-  getCustomers(){
-    this.customers = this.http.get(this.root_url + 'customer');
+  public categories : Category[];
+  public category = {} as Category;
+
+  constructor(private categoryService : CategoryService){
+    this.categories = [];
+  }
+
+  getCategories(){
+    this.categoryService.getAllCategories().subscribe(
+      (response: Category[]) => {
+        this.categories = response;
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  getByIdCategory(){
+    this.categoryService.getByIdCategory().subscribe(
+      (response: Category) => {
+        this.category = response;
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 }
