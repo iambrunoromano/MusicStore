911db251eb74ebb1d2ef3df5291a5355ed5e3bfe
iambrunoromano <brunoromano@hotmail.it>
2021-10-04 17:14:33 +0200

    Get methods for init app
    Removed uneuseful stuff and added methods to fetch categories and products on app init

--- musicstoreapp/src/app/app.component.ts
+++ musicstoreapp/src/app/app.component.ts
@@ -1,28 +1,56 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Category } from './interfaces/category';
+import { Product } from './interfaces/product';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { CategoryService } from './services/category.service';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'musicstoreapp';
   root_url = 'http://localhost:8080/musicstore/api/';
-  public categories : Category[];
-  public category = {} as Category;
 
-  constructor(private categoryService : CategoryService){
-    this.categories = [];
+  public categories : Category[] = [];
+  public category = <Category>{ };
+  public products : Product[] = [];
+  public product = <Product>{ };
+
+  constructor(private categoryService : CategoryService, private productService : ProductService){
+    /*categoryService.getAllCategories().subscribe(data=>{
+      console.warn(data);
+      this.categories = data;
+    });
+    productService.getAllProducts().subscribe(data=>{
+      console.warn(data);
+      this.products = data;
+    });*/
+  }
+
+  ngOnInit() {
+    this.getCategories();
+    this.getProducts();
   }
 
   getCategories(){
     this.categoryService.getAllCategories().subscribe(
       (response: Category[]) => {
         this.categories = response;
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  getProducts(){
+    this.productService.getAllProducts().subscribe(
+      (response: Product[]) => {
+        this.products = response;
       },
       (error : HttpErrorResponse) => {
         alert(error.message);
@@ -40,4 +68,17 @@ export class AppComponent{
       }
     );
   }
+
+  getByIdProduct(){
+    this.productService.getByIdProduct().subscribe(
+      (response: Product) => {
+        this.product = response;
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
 }
