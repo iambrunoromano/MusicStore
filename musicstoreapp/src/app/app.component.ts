85ab5b31606136ae7e23ea1da3071e9f914ae7cf
iambrunoromano <brunoromano@hotmail.it>
2021-10-22 00:16:58 +0200

    Removed uneuseful or moved-eldewhere parts

--- musicstoreapp/src/app/app.component.ts
+++ musicstoreapp/src/app/app.component.ts
@@ -7,8 +7,9 @@ import { Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 import { CategoryService } from './services/category.service';
-import { ProductService } from './services/product.service';
 import { Dropdown } from '../scripts/dropdown';
+
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
@@ -16,73 +17,12 @@ import { Dropdown } from '../scripts/dropdown';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'musicstoreapp';
-  root_url = 'http://localhost:8080/musicstore/api/';
-
-  public categories : Category[] = [];
-  public category = <Category>{ };
-  public products : Product[] = [];
-  public product = <Product>{ };
+  
   public dropdown = new Dropdown();
 
-  constructor(private categoryService : CategoryService, private productService : ProductService, @Inject(DOCUMENT) private document: Document){
-    /*categoryService.getAllCategories().subscribe(data=>{
-      console.warn(data);
-      this.categories = data;
-    });
-    productService.getAllProducts().subscribe(data=>{
-      console.warn(data);
-      this.products = data;
-    });*/
-  }
+  constructor(@Inject(DOCUMENT) private document: Document){}
 
   ngOnInit() {
-    this.getCategories();
-    this.getProducts();
-  }
-
-  getCategories(){
-    this.categoryService.getAllCategories().subscribe(
-      (response: Category[]) => {
-        this.categories = response;
-      },
-      (error : HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
-  }
-
-  getProducts(){
-    this.productService.getAllProducts().subscribe(
-      (response: Product[]) => {
-        this.products = response;
-      },
-      (error : HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
-  }
-
-  getByIdCategory(){
-    this.categoryService.getByIdCategory().subscribe(
-      (response: Category) => {
-        this.category = response;
-      },
-      (error : HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
-  }
-
-  getByIdProduct(){
-    this.productService.getByIdProduct().subscribe(
-      (response: Product) => {
-        this.product = response;
-      },
-      (error : HttpErrorResponse) => {
-        alert(error.message);
-      }
-    );
   }
 
   doDropdown(){
