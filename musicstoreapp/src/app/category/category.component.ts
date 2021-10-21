6a48f3b033427cee066aaa28651dfdec91011f3b
iambrunoromano <brunoromano@hotmail.it>
2021-10-22 00:12:44 +0200

    Category components first implementation

---
+++ musicstoreapp/src/app/category/category.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { Category } from '../interfaces/category';
+import { CategoryService } from '../services/category.service';
+
+@Injectable({
+  providedIn : 'root'
+})
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+
+  public categories: Category[] = [];
+  public category = <Category>{ };
+
+  constructor(private categoryService : CategoryService) {
+  this.getCategories();
+  }
+
+  ngOnInit(): void {
+  }
+
+  public getCategories(): void{
+    this.categoryService.getAllCategories().subscribe(
+      (response: Category[]) => {
+        this.categories = response;
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public getByIdCategory(id: number): void{
+    this.categoryService.getByIdCategory(id).subscribe(
+      (response: Category) => {
+        this.category = response;
+      },
+      (error : HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+}
