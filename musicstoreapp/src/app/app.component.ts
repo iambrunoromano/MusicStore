f0baf82a3928d1894ac5b309a5cb4c1a7a185cea
iambrunoromano <brunoromano@hotmail.it>
2021-10-22 00:17:56 +0200

    Removed unuseful imports

--- musicstoreapp/src/app/app.component.ts
+++ musicstoreapp/src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component,OnInit } from '@angular/core';
-import { Category } from './interfaces/category';
-import { Product } from './interfaces/product';
-import { HttpErrorResponse } from '@angular/common/http';
 
 import { Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
-import { CategoryService } from './services/category.service';
 import { Dropdown } from '../scripts/dropdown';
 
-import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +12,7 @@ import { ProductComponent } from './product/product.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  
+
   public dropdown = new Dropdown();
 
   constructor(@Inject(DOCUMENT) private document: Document){}
