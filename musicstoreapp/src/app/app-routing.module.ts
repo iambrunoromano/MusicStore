34ece42a1d854473121b8afa211a362095be88a0
iambrunoromano <brunoromano@hotmail.it>
2021-10-22 00:19:16 +0200

    Added first Home:[Products, Categories] tree

--- musicstoreapp/src/app/app-routing.module.ts
+++ musicstoreapp/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {path: 'products', component: ProductComponent},
+      {path: 'categories', component: CategoryComponent},
+    ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
