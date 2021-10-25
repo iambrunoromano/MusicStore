2f4de5746447f6997dae8ab01392f52cab9b9757
iambrunoromano <brunoromano@hotmail.it>
2021-10-25 22:44:56 +0200

    Login API all on Login Button click

--- musicstoreapp/src/app/simplecomponents/login/login.component.ts
+++ musicstoreapp/src/app/simplecomponents/login/login.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { LogStatus } from '../../interfaces/logstatus';
+import { WebUser } from '../../interfaces/webuser';
+import { Body } from '../../interfaces/body';
+import { WebUserService } from '../../services/webuser.service';
+
+@Injectable({
+  providedIn : 'root'
+})
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +19,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public logstatus = <LogStatus>{ };
+
+  constructor(private webuserService : WebUserService,
+              private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  public login(): void{
+    let wu : WebUser = {mail:(<HTMLInputElement>document.getElementById("username"))?.value,
+                        password:(<HTMLInputElement>document.getElementById("password"))?.value};
+    /*let body: Body = {authorized: wu};
+    console.log(body.authorized);*/
+    this.webuserService.Login(wu).subscribe(
+      (response: LogStatus) => {
+        this.logstatus = response;
+        this.router.navigate(['/']);
+      },
+      (error : HttpErrorResponse) => {
+        this.router.navigate(['/login']);
+      }
+    );
+  }
 }
