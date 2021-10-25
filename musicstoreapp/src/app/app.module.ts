4b4df1ed10d826be48703e75fd5302f8e834e686
iambrunoromano <brunoromano@hotmail.it>
2021-10-25 22:43:07 +0200

    New Composed Components

--- musicstoreapp/src/app/app.module.ts
+++ musicstoreapp/src/app/app.module.ts
@@ -8,8 +8,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { ProductComponent } from './simplecomponents/product/product.component';
 import { CategoryComponent } from './simplecomponents/category/category.component';
+import { LoginComponent } from './simplecomponents/login/login.component';
+import { RegisterComponent } from './simplecomponents/register/register.component';
+
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+
+import { AllproductsComponent } from './composedcomponents/allproducts/allproducts.component';
+import { AllcategoriesComponent } from './composedcomponents/allcategories/allcategories.component';
+import { SearchComponent } from './composedcomponents/search/search.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +23,19 @@ import { LoginComponent } from './login/login.component';
     ProductComponent,
     CategoryComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    AllproductsComponent,
+    AllcategoriesComponent,
+    RegisterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [AllproductsComponent,
+              AllcategoriesComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
