60b3f2b55de67cf3a8ba3bff47ea66963f6980f0
iambrunoromano <brunoromano@hotmail.it>
2021-10-25 22:42:11 +0200

    Composed Components routing

--- musicstoreapp/src/app/app-routing.module.ts
+++ musicstoreapp/src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ProductComponent } from './product/product.component';
-import { CategoryComponent } from './category/category.component';
+import { AllproductsComponent } from './composedcomponents/allproducts/allproducts.component';
+import { AllcategoriesComponent } from './composedcomponents/allcategories/allcategories.component';
+import { SearchComponent } from './composedcomponents/search/search.component';
+
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './simplecomponents/login/login.component';
+import { RegisterComponent } from './simplecomponents/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     children: [
-      {path: 'products', component: ProductComponent},
-      {path: 'categories', component: CategoryComponent},
+      {path: 'products', component: AllproductsComponent},
+      {path: 'categories', component: AllcategoriesComponent},
+      {path: 'search', component: SearchComponent},
     ]},
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
