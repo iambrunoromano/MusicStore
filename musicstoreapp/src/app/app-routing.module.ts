aa4b7afb443a4f2ca1867ff5ef0d72478b3623bb
iambrunoromano <brunoromano@hotmail.it>
2021-10-26 22:29:21 +0200

    Deletion of components' gerarchy

--- musicstoreapp/src/app/app-routing.module.ts
+++ musicstoreapp/src/app/app-routing.module.ts
@@ -4,22 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AllproductsComponent } from './composedcomponents/allproducts/allproducts.component';
 import { AllcategoriesComponent } from './composedcomponents/allcategories/allcategories.component';
 import { SearchComponent } from './composedcomponents/search/search.component';
+import { BestComponent } from './composedcomponents/best/best.component';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './simplecomponents/login/login.component';
 import { RegisterComponent } from './simplecomponents/register/register.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    children: [
-      {path: 'products', component: AllproductsComponent},
-      {path: 'categories', component: AllcategoriesComponent},
-      {path: 'search', component: SearchComponent},
-    ]},
-    { path: 'login', component: LoginComponent},
-    { path: 'register', component: RegisterComponent},
+  {path: '', component: BestComponent},
+  {path: 'products', component: AllproductsComponent},
+  {path: 'categories', component: AllcategoriesComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
