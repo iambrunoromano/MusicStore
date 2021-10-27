b10a850eaaa35db6727e87c0390205a9f127a522
iambrunoromano <brunoromano@hotmail.it>
2021-10-27 23:05:51 +0200

    Method Fix

--- musicstoreapp/src/app/services/cart.service.ts
+++ musicstoreapp/src/app/services/cart.service.ts
@@ -6,13 +6,14 @@ import { HttpHeaders } from '@angular/common/http';
 import { Body } from '../interfaces/body';
 import { WebUser } from '../interfaces/webuser';
 import { Cart } from '../interfaces/cart';
+import { Product } from '../interfaces/product';
 
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn : 'root'
 })
-export class AdminService{
+export class CartService{
   private root_url = environment.apiBaseUrl;
   private httpOptions = {
     headers: new HttpHeaders({
@@ -23,6 +24,10 @@ export class AdminService{
   private addressAPI: string = "cart";
 
   constructor(private http : HttpClient){}
+
+  public ProductsByCart(wu: WebUser): Observable<Product[]>{
+    return this.http.post<Product[]>(this.root_url + this.addressAPI + '/' + wu.mail + '/products',wu);
+  }
 
   public getAll(wu: WebUser): Observable<Cart[]>{
     return this.http.post<Cart[]>(this.root_url + this.addressAPI + '/all',wu);
