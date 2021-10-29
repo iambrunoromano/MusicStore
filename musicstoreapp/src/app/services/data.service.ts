ac6c8599db30de27c33df9923ad40106dd089b32
iambrunoromano <brunoromano@hotmail.it>
2021-10-29 13:42:31 +0200

    Fix return type and navigation handling

--- musicstoreapp/src/app/services/data.service.ts
+++ musicstoreapp/src/app/services/data.service.ts
@@ -10,7 +10,9 @@ import { Body } from '../interfaces/body';
 import { WebUser } from '../interfaces/webuser';
 import { Product } from '../interfaces/product';
 import { Cart } from '../interfaces/cart';
-import { CartToOrder } from '../interfaces/carttoorder';
+import { OrderReceipt } from '../interfaces/orderreceipt';
+
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn : 'root'
@@ -20,10 +22,11 @@ export class DataService{
   private userData = <WebUser>{ };
   private logStatus = <LogStatus>{ };
   private body = <Body>{ };
-  private lastOrder : CartToOrder[] = [];
+  private lastOrder = <OrderReceipt>{ };
 
   constructor(private cartService : CartService,
-              private orderService : OrderService){
+              private orderService : OrderService,
+              private router : Router){
     this.initUserData();
     this.setLogStatus(false);
   }
@@ -69,8 +72,9 @@ export class DataService{
 
   public gotoorder(): void{
     this.orderService.create(this.userData).subscribe(
-      (response: CartToOrder[]) => {
+      (response: OrderReceipt) => {
         this.lastOrder = response;
+        this.router.navigate(['/order']);
       },
       (error : HttpErrorResponse) => {
         alert(error.message);
@@ -78,7 +82,7 @@ export class DataService{
     );
   }
 
-  public getLastOrder(): CartToOrder[]{
+  public getLastOrder(): OrderReceipt{
     return this.lastOrder;
   }
 
