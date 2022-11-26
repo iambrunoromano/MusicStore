1534adf053a8a2bc10cfefa064aa232fbe209d93
iambrunoromano <brunoromano@hotmail.it>
2022-11-26 23:31:55 +0100

    admin controller useful constants

---
+++ MusicStore/src/main/java/com/musicstore/constant/AdminControllerConstant.java
@@ -0,0 +1,7 @@
+package com.musicstore.constant;
+
+public class AdminControllerConstant {
+  public static final String NOT_ADMIN = "Request by not an admin";
+  public static final String TO_DELETE_NOT_FOUND = "Admin to delete not found";
+  public static final String ADMIN_NOT_FOUND = "Admin not found";
+}
