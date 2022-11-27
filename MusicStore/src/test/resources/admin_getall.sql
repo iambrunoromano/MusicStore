bc59d84f249272b66817930a826049ad3d8a2bb4
iambrunoromano <brunoromano@hotmail.it>
2022-11-27 17:15:00 +0100

    rename

---
+++ MusicStore/src/test/resources/admin_getall.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE admin;
+INSERT INTO admin (mail,name,surname,phone_number) VALUES ("mail1@test","name1","surname1","111-111-111");
+INSERT INTO admin (mail,name,surname,phone_number) VALUES ("mail2@test","name2","surname2","222-222-222");
