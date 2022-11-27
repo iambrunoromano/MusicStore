86ff3369c8c4d229a3818f738c1b6aa78b66bb5e
iambrunoromano <brunoromano@hotmail.it>
2022-11-27 17:30:00 +0100

    added annotation and refactor

--- MusicStore/src/main/java/com/musicstore/model/CartBean.java
+++ MusicStore/src/main/java/com/musicstore/model/CartBean.java
@@ -9,19 +9,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-public @Data @NoArgsConstructor class CartBean {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartBean {
 
-	private int product_id;
-	private int quantity;
-	private String mail;
-	private Timestamp date;
-	
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  private int product_id;
+  private int quantity;
+  private String mail;
+  private Timestamp date;
 }
