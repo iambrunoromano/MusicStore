d80f9acaae27ae3889396a15939ca3f4059b8ac3
iambrunoromano <brunoromano@hotmail.it>
2022-11-27 17:36:00 +0100

    findByMail method

--- MusicStore/src/main/java/com/musicstore/repository/CartRepository.java
+++ MusicStore/src/main/java/com/musicstore/repository/CartRepository.java
@@ -1,9 +1,12 @@
 package com.musicstore.repository;
 
+import com.musicstore.entity.Cart;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.musicstore.entity.Cart;
+import java.util.List;
 
 @Repository
-public interface CartRepository extends CrudRepository<Cart, Integer> {}
+public interface CartRepository extends CrudRepository<Cart, Integer> {
+  List<Cart> findByMail(String mail);
+}
