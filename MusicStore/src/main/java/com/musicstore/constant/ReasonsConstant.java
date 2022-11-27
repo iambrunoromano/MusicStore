f385459181ae5826f032dac0d514c2fee1c8e5e9
iambrunoromano <brunoromano@hotmail.it>
2022-11-27 11:05:11 +0100

    rename and pruning

---
+++ MusicStore/src/main/java/com/musicstore/constant/ReasonsConstant.java
@@ -0,0 +1,6 @@
+package com.musicstore.constant;
+
+public class ReasonsConstant {
+  public static final String NOT_USER = "Request by a non user";
+  public static final String NOT_ADMIN = "Request by not an admin";
+}
