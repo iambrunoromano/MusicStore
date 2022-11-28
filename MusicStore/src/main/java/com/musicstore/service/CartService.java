3f5b9e5f0d658517650e97c35d1910dd69967669
iambrunoromano <brunoromano@hotmail.it>
2022-11-28 19:13:00 +0100

    deleteByMail no more boolean

--- MusicStore/src/main/java/com/musicstore/service/CartService.java
+++ MusicStore/src/main/java/com/musicstore/service/CartService.java
@@ -1,10 +1,13 @@
 package com.musicstore.service;
 
+import com.musicstore.constant.ReasonsConstant;
 import com.musicstore.entity.Cart;
 import com.musicstore.repository.CartRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -31,14 +34,14 @@ public class CartService {
     return cartRepository.save(cart);
   }
 
-  public boolean deleteByMail(String mail) {
+  public void deleteByMail(String mail) {
     List<Cart> cartList = this.getByMail(mail);
     if (cartList.isEmpty()) {
-      return false;
+      log.info("No cart rows found for user with mail [{}]", mail);
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, ReasonsConstant.CART_NOT_FOUND);
     }
     for (Cart cart : cartList) {
       cartRepository.delete(cart);
     }
-    return true;
   }
 }
