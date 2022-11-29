e8322d823e75713238131e3969622debf86609e2
iambrunoromano <brunoromano@hotmail.it>
2022-11-29 18:51:00 +0100

    method to test right exception throw

--- MusicStore/src/test/java/com/musicstore/service/UserServiceTest.java
+++ MusicStore/src/test/java/com/musicstore/service/UserServiceTest.java
@@ -1,2 +1,16 @@
-package com.musicstore.service;public class UserServiceTest {
+package com.musicstore.service;
+
+import com.musicstore.constant.ReasonsConstant;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserServiceTest {
+  public static void assertNotUserException(ResponseStatusException actualException) {
+    ResponseStatusException expectedException =
+        new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_USER);
+    assertEquals(expectedException.getReason(), actualException.getReason());
+    assertEquals(expectedException.getStatus(), actualException.getStatus());
+  }
 }
