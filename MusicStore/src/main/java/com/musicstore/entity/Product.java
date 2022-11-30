149ffe403aec156be705779885ab680090d28598
iambrunoromano <brunoromano@hotmail.it>
2022-11-30 19:23:00 +0100

    refactor

--- MusicStore/src/main/java/com/musicstore/entity/Product.java
+++ MusicStore/src/main/java/com/musicstore/entity/Product.java
@@ -1,81 +1,39 @@
-package com.musicstore.model;
+package com.musicstore.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.NamedStoredProcedureQueries;
-import javax.persistence.NamedStoredProcedureQuery;
-import javax.persistence.SqlResultSetMapping;
-import javax.persistence.StoredProcedureParameter;
-import javax.persistence.ConstructorResult;
-import javax.persistence.ColumnResult;
-import javax.persistence.ParameterMode;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import com.musicstore.pojos.BestProducts;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Entity
-@SqlResultSetMapping(
-    name = "firstProductProcMapping",
-    classes = {
-      @ConstructorResult(
-          targetClass = BestProducts.class,
-          columns = {
-            @ColumnResult(name = "id", type = Integer.class),
-            @ColumnResult(name = "name", type = String.class),
-            @ColumnResult(name = "price", type = Double.class),
-            @ColumnResult(name = "quantity", type = Integer.class),
-            @ColumnResult(name = "producer", type = String.class),
-            @ColumnResult(name = "category", type = Integer.class),
-            @ColumnResult(name = "imgurl", type = String.class)
-          })
-    })
-@NamedStoredProcedureQueries({
-  @NamedStoredProcedureQuery(
-      name = "productFirstProc",
-      procedureName = "BestProducts",
-      resultSetMappings = {"firstProductProcMapping"}),
-  @NamedStoredProcedureQuery(
-      name = "productSecondProc",
-      procedureName = "ProductsByProducer",
-      parameters = {
-        @StoredProcedureParameter(
-            mode = ParameterMode.IN,
-            name = "producerMail",
-            type = String.class)
-      },
-      resultSetMappings = {"firstProductProcMapping"}),
-  @NamedStoredProcedureQuery(
-      name = "productThirdProc",
-      procedureName = "ProductsByCart",
-      parameters = {
-        @StoredProcedureParameter(mode = ParameterMode.IN, name = "user_mail", type = String.class)
-      },
-      resultSetMappings = {"firstProductProcMapping"}),
-  @NamedStoredProcedureQuery(
-      name = "productFourthProc",
-      procedureName = "ProductsByCategory",
-      parameters = {
-        @StoredProcedureParameter(
-            mode = ParameterMode.IN,
-            name = "categoryId",
-            type = Integer.class)
-      },
-      resultSetMappings = {"firstProductProcMapping"})
-})
-public @Data @NoArgsConstructor class Product {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
   private int id;
 
+  @Column(name = "name")
   private String name;
+
+  @Column(name = "price")
   private double price;
+
+  @Column(name = "quantity")
   private int quantity;
+
+  @Column(name = "producer")
   private String producer;
+
+  @Column(name = "category")
   private int category;
-  private String imgurl;
+
+  @Column(name = "img_url")
+  private String imgUrl;
 }
