71ffb2fd75636b230f9bd96cbc802472b8919b93
iambrunoromano <brunoromano@hotmail.it>
2022-11-30 19:28:00 +0100

    needed reasons

--- MusicStore/src/main/java/com/musicstore/constant/ReasonsConstant.java
+++ MusicStore/src/main/java/com/musicstore/constant/ReasonsConstant.java
@@ -3,4 +3,7 @@ package com.musicstore.constant;
 public class ReasonsConstant {
   public static final String NOT_USER = "Request by a non user";
   public static final String NOT_ADMIN = "Request by not an admin";
+  public static final String CART_NOT_FOUND = "Cart not found";
+  public static final String CATEGORY_NOT_FOUND = "Category not found";
+  public static final String PRODUCT_NOT_FOUND = "Product not found";
 }
