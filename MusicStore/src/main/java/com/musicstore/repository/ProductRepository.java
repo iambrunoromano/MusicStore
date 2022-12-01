afbed2700bcff742bc6252075644297e562e2e2e
iambrunoromano <brunoromano@hotmail.it>
2022-12-01 19:17:27 +0100

    query method

--- MusicStore/src/main/java/com/musicstore/repository/ProductRepository.java
+++ MusicStore/src/main/java/com/musicstore/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.musicstore.repository;
 
 import com.musicstore.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,7 @@ public interface ProductRepository extends CrudRepository<Product, Integer> {
   List<Product> findByCategory(Integer category);
 
   List<Product> findByOrderBySoldQuantityDesc();
+
+  @Query("SELECT DISTINCT(category) FROM product WHERE producer = ?1")
+  List<Integer> findCategoriesByProducer(String producer);
 }
