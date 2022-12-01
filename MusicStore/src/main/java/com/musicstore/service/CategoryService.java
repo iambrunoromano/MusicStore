5214efa8feae15d534c2c2dadd00605ba2a8c39a
iambrunoromano <brunoromano@hotmail.it>
2022-12-01 19:12:07 +0100

    refactor getByProducer method

--- MusicStore/src/main/java/com/musicstore/service/CategoryService.java
+++ MusicStore/src/main/java/com/musicstore/service/CategoryService.java
@@ -3,7 +3,7 @@ package com.musicstore.service;
 import com.musicstore.constant.ReasonsConstant;
 import com.musicstore.entity.Category;
 import com.musicstore.repository.CategoryRepository;
-import com.musicstore.repository.ProducerRepository;
+import com.musicstore.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,12 +18,16 @@ import java.util.Optional;
 @Slf4j
 public class CategoryService {
 
+  // TODO: unit tests
+
   private final CategoryRepository categoryRepository;
+  private final ProductRepository productRepository;
 
   @Autowired
   public CategoryService(
-      CategoryRepository categoryRepository, ProducerRepository producerRepository) {
+      CategoryRepository categoryRepository, ProductRepository productRepository) {
     this.categoryRepository = categoryRepository;
+    this.productRepository = productRepository;
   }
 
   public Iterable<Category> getAll() {
@@ -48,13 +52,20 @@ public class CategoryService {
     throw new ResponseStatusException(HttpStatus.NOT_FOUND, ReasonsConstant.CATEGORY_NOT_FOUND);
   }
 
-  public List<Category> getAllByProducer(String mail) {
-    // TODO: implement this method once product track is on. Guide:
-    // 1. Query all products with product.producer = mail (launch exception if mail is not producer)
-    // 2. Select distinct categories from all those products (launch exception if any product is
-    // found for that producer)
-    // 3. Query those distinct categories on categories table and give back to the caller (launch
-    // exception if no category is found for products)
-    return new ArrayList<>();
+  public List<Category> getByProducer(String mail) {
+    List<Integer> categoryIdList = productRepository.findCategoriesByProducer(mail);
+    List<Category> categoryList = new ArrayList<>();
+    for (Integer categoryId : categoryIdList) {
+      Optional<Category> optionalCategory = categoryRepository.findById(categoryId);
+      if (!optionalCategory.isPresent()) {
+        log.error(
+            "Category with id [{}] of product belonging to producer with mail [{}] not found",
+            categoryId,
+            mail);
+      } else {
+        categoryList.add(optionalCategory.get());
+      }
+    }
+    return categoryList;
   }
 }
