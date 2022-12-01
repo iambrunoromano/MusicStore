f55ca37b56bb05e92d0181ae12e44319e851c8af
iambrunoromano <brunoromano@hotmail.it>
2022-12-01 18:47:12 +0100

    refactor

--- MusicStore/src/main/java/com/musicstore/service/ProductService.java
+++ MusicStore/src/main/java/com/musicstore/service/ProductService.java
@@ -1,43 +1,28 @@
 package com.musicstore.service;
 
+import com.musicstore.constant.ReasonsConstant;
+import com.musicstore.entity.Product;
+import com.musicstore.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.musicstore.repository.ProductRepository;
-import com.musicstore.entity.Product;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.StoredProcedureQuery;
-
 @Service
+@Slf4j
 public class ProductService {
 
-  @Autowired private ProductRepository productRepository;
+  // TODO: unit tests
 
-  @PersistenceContext private EntityManager em;
+  private final ProductRepository productRepository;
 
-  public List<Product> ProductsByCategory(int id) {
-    StoredProcedureQuery spq = em.createNamedStoredProcedureQuery("productFourthProc");
-    spq.setParameter("categoryId", id);
-    spq.execute();
-    return spq.getResultList();
-  }
-
-  public List<Product> ProductsByProducer(String mail) {
-    StoredProcedureQuery spq = em.createNamedStoredProcedureQuery("productSecondProc");
-    spq.setParameter("producerMail", mail);
-    spq.execute();
-    return spq.getResultList();
-  }
-
-  public List<Product> BestProducts() {
-    StoredProcedureQuery spq = em.createNamedStoredProcedureQuery("productFirstProc");
-    spq.execute();
-    return spq.getResultList();
+  @Autowired
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
   }
 
   public Iterable<Product> getAll() {
@@ -48,32 +33,28 @@ public class ProductService {
     return productRepository.findById(id);
   }
 
-  public Product create(Product p) {
+  public Product save(Product p) {
     return productRepository.save(p);
   }
 
-  public Optional<Product> update(int id, Product p) {
-    Optional<Product> foundProduct = productRepository.findById(id);
-    if (!foundProduct.isPresent()) {
-      return Optional.empty();
+  public boolean delete(int id) {
+    Optional<Product> optionalProduct = productRepository.findById(id);
+    if (optionalProduct.isPresent()) {
+      log.info("Deleting product with id [{}]", id);
+      productRepository.delete(optionalProduct.get());
     }
-
-    foundProduct.get().setName(p.getName());
-    foundProduct.get().setPrice(p.getPrice());
-    foundProduct.get().setQuantity(p.getQuantity());
-    foundProduct.get().setProducer(p.getProducer());
-    foundProduct.get().setCategory(p.getCategory());
-
-    productRepository.save(foundProduct.get());
-    return foundProduct;
+    throw new ResponseStatusException(HttpStatus.NOT_FOUND, ReasonsConstant.PRODUCT_NOT_FOUND);
   }
 
-  public boolean delete(int id) {
-    Optional<Product> foundProduct = productRepository.findById(id);
-    if (!foundProduct.isPresent()) {
-      return false;
-    }
-    productRepository.delete(foundProduct.get());
-    return false;
+  public List<Product> getByProducer(String mail) {
+    return productRepository.findByProducer(mail);
+  }
+
+  public List<Product> getByCategory(Integer category) {
+    return productRepository.findByCategory(category);
+  }
+
+  public List<Product> getMostSold() {
+    return productRepository.findByOrderBySoldQuantityDesc();
   }
 }
