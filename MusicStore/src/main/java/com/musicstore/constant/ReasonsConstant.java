92570932171c2544b062cab1266c82e5bc95f098
iambrunoromano <brunoromano@hotmail.it>
2022-12-03 09:43:13 +0100

    non-auth

--- MusicStore/src/main/java/com/musicstore/constant/ReasonsConstant.java
+++ MusicStore/src/main/java/com/musicstore/constant/ReasonsConstant.java
@@ -1,6 +1,7 @@
 package com.musicstore.constant;
 
 public class ReasonsConstant {
+  public static final String NOT_AUTHENTIC = "Request by a non-authentic user";
   public static final String NOT_USER = "Request by a non user";
   public static final String NOT_ADMIN = "Request by not an admin";
   public static final String NOT_PRODUCER = "Request by a non producer";
