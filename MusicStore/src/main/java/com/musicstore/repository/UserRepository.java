44ccbc5c52f758eb3dba069a48e3e454aa5cbd99
iambrunoromano <brunoromano@hotmail.it>
2022-12-03 09:46:09 +0100

    auth repo method

--- MusicStore/src/main/java/com/musicstore/repository/UserRepository.java
+++ MusicStore/src/main/java/com/musicstore/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.musicstore.repository;
 
+import com.musicstore.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.musicstore.entity.User;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {}
+public interface UserRepository extends CrudRepository<User, String> {
+  Optional<User> findByMailAndPassword(String mail, String password);
+}
