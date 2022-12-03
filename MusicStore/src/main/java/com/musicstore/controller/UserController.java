7fa59cde81875b101168ba62b4bf81b9291dd264
iambrunoromano <brunoromano@hotmail.it>
2022-12-03 11:33:24 +0100

    refactor and testing

--- MusicStore/src/main/java/com/musicstore/controller/UserController.java
+++ MusicStore/src/main/java/com/musicstore/controller/UserController.java
@@ -1,104 +1,54 @@
 package com.musicstore.controller;
 
-import java.util.Map;
-import java.util.Optional;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.server.ResponseStatusException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.musicstore.entity.User;
 import com.musicstore.service.AdminService;
 import com.musicstore.service.UserService;
-import com.musicstore.utility.Utility;
-import com.musicstore.utility.LoggedIn;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+// TODO: integration test
+// TODO: external test
+// TODO: Returned Response Entity HTTP Status
 
 @RestController
-public class WebUserRestController {
+@Slf4j
+@RequestMapping(value = "user")
+public class UserController {
 
-  @Autowired private AdminService adminService;
+  private final AdminService adminService;
+  private final UserService userService;
 
-  @Autowired private UserService webuserService;
-
-  public WebUserRestController() {}
-
-  @RequestMapping(value = "/musicstore/api/webuser/all", method = RequestMethod.POST)
-  public Iterable<User> getAll(@RequestBody User b) {
-    adminService.isAdmin(b.getMail());
-    return webuserService.getAll();
+  @Autowired
+  public UserController(AdminService adminService, UserService userService) {
+    this.adminService = adminService;
+    this.userService = userService;
   }
 
-  @RequestMapping(value = "/musicstore/api/webuser/{id}", method = RequestMethod.POST)
-  public User getById(@PathVariable String id, @RequestBody User b) {
-    adminService.isAdmin(b.getMail());
-    /*if (!webuserService.isAuthentic(b)) {
-      throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, "request by not an admin");
-    }*/
-    return webuserService.getById(id).get();
+  @GetMapping(value = "/all/{admin-id}")
+  public Iterable<User> getAll(@PathVariable String adminId) {
+    adminService.isAdmin(adminId);
+    return userService.getAll();
   }
 
-  @RequestMapping(value = "/musicstore/api/webuserlogin", method = RequestMethod.POST)
-  public LoggedIn login(@RequestBody User b) {
-    LoggedIn logged = new LoggedIn();
-    if (b.getMail().equals(webuserService.getById(b.getMail()).get().getMail())
-        && b.getPassword().equals(webuserService.getById(b.getMail()).get().getPassword())) {
-      logged.setLogstatus(true);
-    } else {
-      logged.setLogstatus(false);
-    }
-    return logged;
+  @GetMapping
+  public User getById(@RequestBody User user) {
+    // TODO: since this method can be used as a login wrap the User object into a 200 HttpStatus
+    // Response Obj
+    // TODO: on the frontend when a user logs in and the response is positive the frontend logic
+    // component should store username & password and manage them for the next calls the user wants
+    // to do providing them to the called in the right place
+    return userService.isAuthentic(user);
   }
 
-  @RequestMapping(value = "/musicstore/api/webuserlogout", method = RequestMethod.POST)
-  public LoggedIn logout(@RequestBody User b) {
-    LoggedIn logged = new LoggedIn();
-    if (b.getMail().equals(webuserService.getById(b.getMail()).get().getMail())
-        && b.getPassword().equals(webuserService.getById(b.getMail()).get().getPassword())) {
-      logged.setLogstatus(false);
-    } else {
-      logged.setLogstatus(true);
-    }
-    return logged;
+  @PostMapping
+  public User update(@RequestBody User user) {
+    return userService.save(user);
   }
 
-  @RequestMapping(value = "/musicstore/api/webuser", method = RequestMethod.POST)
-  public User create(@RequestBody User p) {
-    return webuserService.save(p);
-  }
-
-  @RequestMapping(value = "/musicstore/api/webuser/{id}", method = RequestMethod.PUT)
-  public User update(@PathVariable String id, @RequestBody Map<String, Map<String, String>> map) {
-    User wub = Utility.webuserDeMap(map.get("toput"));
-    User b = Utility.webuserDeMap(map.get("authorized"));
-    adminService.isAdmin(b.getMail());
-    webuserService.isAuthentic(b);
-    if (!b.getMail().equals(wub.getMail())) {
-      throw new ResponseStatusException(
-          HttpStatus.METHOD_NOT_ALLOWED, "request by not an authorized user");
-    }
-    User updatedWebUser = webuserService.save(wub);
-    /*if (!updatedWebUser.isPresent()) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "item not found");
-    }*/
-    return new User(); // updatedWebUser.get();
-  }
-
-  @RequestMapping(value = "/musicstore/api/webuser/{id}", method = RequestMethod.DELETE)
-  public void delete(@PathVariable String id, @RequestBody User b) {
-    adminService.isAdmin(b.getMail());
-    webuserService.isAuthentic(b);
-    if (!b.getMail().equals(webuserService.getById(id).get().getMail())) {
-      throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, "request by not an admin");
-    }
-    webuserService.delete(id);
-    /*if (isDeleted == false) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "item not found");
-    }*/
+  @DeleteMapping(value = "/{user-id}")
+  public void delete(@PathVariable String userId) {
+    userService.isUser(userId);
+    userService.delete(userId);
   }
 }
--- MusicStore/src/test/java/com/musicstore/controller/UserControllerTest.java
+++ MusicStore/src/test/java/com/musicstore/controller/UserControllerTest.java
@@ -1,4 +1,129 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.musicstore.controller;
+
+import com.musicstore.constant.ReasonsConstant;
+import com.musicstore.entity.User;
+import com.musicstore.service.AdminService;
+import com.musicstore.service.AdminServiceTest;
+import com.musicstore.service.UserService;
+import com.musicstore.service.UserServiceTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class UserControllerTest {
-  
+
+  public static final String NOT_ADMIN_ID = "not-admin-id";
+  public static final String ADMIN_ID = "admin-id";
+
+  private AdminService adminService = Mockito.mock(AdminService.class);
+  private UserService userService = Mockito.mock(UserService.class);
+  private UserController userController = new UserController(adminService, userService);
+
+  @Test
+  void getAllNotAdminTest() {
+    mockIsNotAdmin();
+    ResponseStatusException actualException =
+        assertThrows(
+            ResponseStatusException.class,
+            () -> {
+              userController.getAll(NOT_ADMIN_ID);
+            });
+    AdminServiceTest.assertNotAdminException(actualException);
+  }
+
+  @Test
+  void getAllTest() {
+    mockIsAdmin();
+    mockUserList();
+    assertEquals(buildUserList(), userController.getAll(ADMIN_ID));
+  }
+
+  @Test
+  void getByIdTest() {
+    mockIsAuthentic();
+    assertEquals(UserServiceTest.buildUser(), userController.getById(UserServiceTest.buildUser()));
+  }
+
+  @Test
+  void getByIdNotAuthenticTest() {
+    mockIsNotAuthentic();
+    ResponseStatusException actualException =
+        assertThrows(
+            ResponseStatusException.class,
+            () -> {
+              userController.getById(UserServiceTest.buildUser());
+            });
+    UserServiceTest.assertNotAuthenticException(actualException);
+  }
+
+  @Test
+  void saveTest() {
+    mockSave();
+    assertEquals(UserServiceTest.buildUser(), userController.update(UserServiceTest.buildUser()));
+  }
+
+  @Test
+  void deleteNotUserTest() {
+    mockIsNotUser();
+    ResponseStatusException actualException =
+        assertThrows(
+            ResponseStatusException.class,
+            () -> {
+              userController.delete(NOT_ADMIN_ID);
+            });
+    UserServiceTest.assertNotUserException(actualException);
+  }
+
+  private void mockIsNotUser() {
+    ResponseStatusException expectedException =
+        new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_USER);
+    BDDMockito.given(userService.isUser(Mockito.anyString())).willThrow(expectedException);
+  }
+
+  private void mockIsNotAdmin() {
+    ResponseStatusException expectedException =
+        new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_ADMIN);
+    BDDMockito.given(adminService.isAdmin(Mockito.anyString())).willThrow(expectedException);
+  }
+
+  private void mockIsAdmin() {
+    BDDMockito.given(adminService.isAdmin(Mockito.anyString()))
+        .willReturn(AdminServiceTest.buildAdmin());
+  }
+
+  private void mockUserList() {
+    List<User> userList = buildUserList();
+    BDDMockito.given(userService.getAll()).willReturn(userList);
+  }
+
+  private List<User> buildUserList() {
+    List<User> userList = new ArrayList<>();
+    userList.add(UserServiceTest.buildUser());
+    userList.add(UserServiceTest.buildUser());
+    return userList;
+  }
+
+  private void mockIsAuthentic() {
+    BDDMockito.given(userService.isAuthentic(Mockito.any()))
+        .willReturn(UserServiceTest.buildUser());
+  }
+
+  private void mockIsNotAuthentic() {
+    BDDMockito.given(userService.isAuthentic(Mockito.any()))
+        .willThrow(
+            new ResponseStatusException(
+                HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_AUTHENTIC));
+  }
+
+  private void mockSave() {
+    BDDMockito.given(userService.save(Mockito.any())).willReturn(UserServiceTest.buildUser());
+  }
 }
