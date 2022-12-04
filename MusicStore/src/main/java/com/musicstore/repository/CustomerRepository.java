15e225b5f925407da6e9d9cecf4469f071366713
iambrunoromano <brunoromano@hotmail.it>
2022-12-04 09:15:12 +0100

    rename

--- MusicStore/src/main/java/com/musicstore/repository/CustomerRepository.java
+++ MusicStore/src/main/java/com/musicstore/repository/CustomerRepository.java
@@ -1,10 +1,8 @@
 package com.musicstore.repository;
 
+import com.musicstore.entity.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.musicstore.model.CustomerBean;
-
 @Repository
-public interface ICustomerRepository extends CrudRepository<CustomerBean, String>{
-}
+public interface CustomerRepository extends CrudRepository<Customer, String> {}
