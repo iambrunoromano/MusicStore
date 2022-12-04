aad3a2c80c09e92c6eb5c7132632dd6fe3a29da0
iambrunoromano <brunoromano@hotmail.it>
2022-12-04 10:04:01 +0100

    CUSTOMER_NOT_FOUND

--- MusicStore/src/main/java/com/musicstore/constant/ReasonsConstant.java
+++ MusicStore/src/main/java/com/musicstore/constant/ReasonsConstant.java
@@ -7,5 +7,6 @@ public class ReasonsConstant {
   public static final String NOT_PRODUCER = "Request by a non producer";
   public static final String CART_NOT_FOUND = "Cart not found";
   public static final String CATEGORY_NOT_FOUND = "Category not found";
+  public static final String CUSTOMER_NOT_FOUND = "Customer not found";
   public static final String PRODUCT_NOT_FOUND = "Product not found";
 }
