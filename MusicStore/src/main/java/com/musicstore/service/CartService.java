b98202f5db72c9cda45e99263a567e156da40beb
Bruno <brunoromano@hotmail.it>
2023-01-29 16:15:26 +0100

    save refactor and new method to create cart by request

--- MusicStore/src/main/java/com/musicstore/service/CartService.java
+++ MusicStore/src/main/java/com/musicstore/service/CartService.java
@@ -2,24 +2,31 @@ package com.musicstore.service;
 
 import com.musicstore.constant.ReasonsConstant;
 import com.musicstore.entity.Cart;
+import com.musicstore.entity.Product;
 import com.musicstore.repository.CartRepository;
+import com.musicstore.repository.ProductRepository;
+import com.musicstore.request.CartRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
 public class CartService {
 
   private final CartRepository cartRepository;
+  private final ProductRepository productRepository;
 
   @Autowired
-  public CartService(CartRepository cartRepository) {
+  public CartService(CartRepository cartRepository, ProductRepository productRepository) {
     this.cartRepository = cartRepository;
+    this.productRepository = productRepository;
   }
 
   public Iterable<Cart> getAll() {
@@ -34,8 +41,12 @@ public class CartService {
     return cartRepository.findByOrderId(orderId);
   }
 
-  public Cart save(Cart cart) {
-    return cartRepository.save(cart);
+  public void save(List<CartRequest> cartRequestList) {
+    List<Cart> cartList = new ArrayList<>();
+    for (CartRequest cartRequest : cartRequestList) {
+      cartList.add(createCart(cartRequest));
+    }
+    cartRepository.saveAll(cartList);
   }
 
   public boolean deleteByMail(String mail) {
@@ -48,5 +59,20 @@ public class CartService {
       cartRepository.delete(cart);
     }
     return true;
+  }
+
+  public Cart createCart(CartRequest cartRequest) {
+    Optional<Product> optionalProduct = productRepository.findById(cartRequest.getProductId());
+    if (optionalProduct.isPresent() && optionalProduct.get().getPrice() != null) {
+      return Cart.builder()
+          .productId(cartRequest.getProductId())
+          .quantity(cartRequest.getQuantity())
+          .mail(cartRequest.getMail())
+          .date(cartRequest.getDate())
+          .bought(false)
+          .overallPrice(optionalProduct.get().getPrice() * cartRequest.getQuantity())
+          .build();
+    }
+    throw new ResponseStatusException(HttpStatus.NOT_FOUND, ReasonsConstant.PRODUCT_NOT_FOUND);
   }
 }
