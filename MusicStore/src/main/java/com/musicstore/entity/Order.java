1882b7d78ea92ea57f4837e61f46e29307587415
Bruno <brunoromano@hotmail.it>
2023-01-29 12:31:38 +0100

    refactor

--- MusicStore/src/main/java/com/musicstore/entity/Order.java
+++ MusicStore/src/main/java/com/musicstore/entity/Order.java
@@ -1,51 +1,31 @@
 package com.musicstore.entity;
 
-import java.sql.Timestamp;
-import java.util.Objects;
-
-import javax.persistence.ColumnResult;
-import javax.persistence.ConstructorResult;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedStoredProcedureQueries;
-import javax.persistence.NamedStoredProcedureQuery;
-import javax.persistence.SqlResultSetMapping;
-import javax.persistence.StoredProcedureParameter;
-import javax.persistence.ParameterMode;
-
-import com.musicstore.pojos.CartToOrderBI;
-import java.sql.Date;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@SqlResultSetMapping(name="firstOrderProdMapping", classes= {
-		@ConstructorResult(targetClass = CartToOrderBI.class,
-				   columns = {
-						   @ColumnResult(name="mail", type = String.class),
-						   @ColumnResult(name="productId", type = Integer.class),
-						   @ColumnResult(name="orderId", type = Integer.class),
-						   @ColumnResult(name="quantity", type = Integer.class),
-						   @ColumnResult(name="price", type = Double.class)
-				   })
-})
-@NamedStoredProcedureQueries({
-	@NamedStoredProcedureQuery(
-			name="orderFirstProc", 
-			procedureName="CartToOrder", 
-			parameters= {@StoredProcedureParameter(mode=ParameterMode.IN, name="user_mail", type=String.class)}, 
-			resultSetMappings = {"firstOrderProdMapping"})})
-public @Data @NoArgsConstructor class OrderBean {
+import javax.persistence.*;
+import java.sql.Timestamp;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
-	private String mail;
-	private Timestamp date;
-	private double total;
-	
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  @Column(name = "mail")
+  private String mail;
+
+  @Column(name = "date")
+  private Timestamp date;
+
+  @Column(name = "total")
+  private double total;
 }
