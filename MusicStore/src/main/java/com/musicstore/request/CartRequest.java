e1a119d0cf23bc2622e5a1840a1a842bd2d1bc4d
Bruno <brunoromano@hotmail.it>
2023-01-29 16:16:56 +0100

    builder useful for tests

--- MusicStore/src/main/java/com/musicstore/request/CartRequest.java
+++ MusicStore/src/main/java/com/musicstore/request/CartRequest.java
@@ -1,10 +1,12 @@
 package com.musicstore.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.sql.Timestamp;
 
 @Data
+@Builder
 public class CartRequest {
   private int productId;
   private int quantity;
