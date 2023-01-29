bac429bfce52b5cf4e8f00e00eef2b9ae60e1c1a
Bruno <brunoromano@hotmail.it>
2023-01-29 12:24:37 +0100

    repository refactor

--- MusicStore/src/main/java/com/musicstore/repository/OrderRepository.java
+++ MusicStore/src/main/java/com/musicstore/repository/OrderRepository.java
@@ -1,10 +1,8 @@
 package com.musicstore.repository;
 
+import com.musicstore.model.OrderBean;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.musicstore.model.OrderBean;
-
 @Repository
-public interface IOrderRepository extends CrudRepository<OrderBean, Integer>{
-}
+public interface OrderRepository extends CrudRepository<OrderBean, Integer> {}
