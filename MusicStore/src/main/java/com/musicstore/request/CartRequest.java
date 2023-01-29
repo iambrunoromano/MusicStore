81a808c7710ba285a5eabd797f90b9af4a01060b
Bruno <brunoromano@hotmail.it>
2023-01-29 16:24:22 +0100

    removed because it has to be created by backedn

--- MusicStore/src/main/java/com/musicstore/request/CartRequest.java
+++ MusicStore/src/main/java/com/musicstore/request/CartRequest.java
@@ -3,13 +3,10 @@ package com.musicstore.request;
 import lombok.Builder;
 import lombok.Data;
 
-import java.sql.Timestamp;
-
 @Data
 @Builder
 public class CartRequest {
   private int productId;
   private int quantity;
   private String mail;
-  private Timestamp date;
 }
