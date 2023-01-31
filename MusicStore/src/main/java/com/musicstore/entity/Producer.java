16ce4ba0a945657c5bb3e0ea60116bb76d0f30de
Bruno <brunoromano@hotmail.it>
2023-01-31 07:03:31 +0100

    refactor

--- MusicStore/src/main/java/com/musicstore/entity/Producer.java
+++ MusicStore/src/main/java/com/musicstore/entity/Producer.java
@@ -1,35 +1,28 @@
 package com.musicstore.entity;
 
-import javax.persistence.ColumnResult;
-import javax.persistence.ConstructorResult;
-import javax.persistence.Entity;
-import javax.persistence.NamedStoredProcedureQueries;
-import javax.persistence.NamedStoredProcedureQuery;
-import javax.persistence.SqlResultSetMapping;
-
-import javax.persistence.Id;
-
-import com.musicstore.pojos.BestProducers;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@SqlResultSetMapping(name="firstProducerProdMapping", classes= {
-		@ConstructorResult(targetClass = BestProducers.class,
-						   columns = {
-								   @ColumnResult(name="mail", type = String.class),
-								   @ColumnResult(name="name", type = String.class),
-								   @ColumnResult(name="address", type = String.class)
-						   })
-})
-@NamedStoredProcedureQueries({@NamedStoredProcedureQuery(name="producerFirstProc", procedureName="BestProducers", resultSetMappings = {"firstProducerProdMapping"})})
-public @Data @NoArgsConstructor class Producer {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-	@Id
-	private String mail;
-	
-	private String name;
-	private String address;
-	
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Producer {
+
+  @Id
+  @Column(name = "mail")
+  private String mail;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "address")
+  private String address;
 }
