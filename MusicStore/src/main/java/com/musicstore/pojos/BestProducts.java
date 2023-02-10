d761dba7ce3910f996b1e05266b471b0229bdf5a
Bruno <brunoromano@hotmail.it>
2023-02-10 20:51:30 +0100

    todo

--- MusicStore/src/main/java/com/musicstore/pojos/BestProducts.java
+++ MusicStore/src/main/java/com/musicstore/pojos/BestProducts.java
@@ -3,6 +3,8 @@ package com.musicstore.pojos;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 
+// TODO: delete all pojos
+
 public @Data @AllArgsConstructor class BestProducts {
 	
 	private Integer id;
