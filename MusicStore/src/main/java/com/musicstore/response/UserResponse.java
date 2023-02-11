40b3d6941d543d99230744851ac8bf6ce46661d3
Bruno <brunoromano@hotmail.it>
2023-02-11 15:47:24 +0100

    new user response

---
+++ MusicStore/src/main/java/com/musicstore/response/UserResponse.java
@@ -0,0 +1,11 @@
+package com.musicstore.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserResponse {
+    private String mail;
+    private String imgUrl;
+}
