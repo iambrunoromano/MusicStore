1b91bbffa602a60673b287bbf1437508df4d4d9c
Bruno <brunoromano@hotmail.it>
2023-02-11 16:07:07 +0100

    asserting user response/s

--- MusicStore/src/test/java/com/musicstore/integration/UserIntegrationTest.java
+++ MusicStore/src/test/java/com/musicstore/integration/UserIntegrationTest.java
@@ -3,6 +3,7 @@ package com.musicstore.integration;
 import com.musicstore.MusicStoreApplication;
 import com.musicstore.controller.UserController;
 import com.musicstore.entity.User;
+import com.musicstore.response.UserResponse;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,24 +39,25 @@ public class UserIntegrationTest {
   @Order(1)
   @Sql("classpath:integration/user.sql")
   void getAllTest() {
-    List<User> userList = userController.getAll(FIRST_ADMIN_ID);
-    assertEquals(2, userList.size());
+    List<UserResponse> userResponseList = userController.getAll(FIRST_ADMIN_ID);
+    assertEquals(2, userResponseList.size());
   }
 
   @Test
   @Order(2)
   @Sql("classpath:integration/user.sql")
   void getByIdTest() {
-    User user = userController.getById(buildUser(FIRST_USER_ID, FIRST_USER_PASSWORD));
-    assertEquals(FIRST_USER_ID, user.getMail());
+    UserResponse userResponse =
+        userController.getById(buildUser(FIRST_USER_ID, FIRST_USER_PASSWORD));
+    assertEquals(FIRST_USER_ID, userResponse.getMail());
   }
 
   @Test
   @Order(3)
   @Sql("classpath:integration/user.sql")
   void saveTest() {
-    User user = userController.save(buildUser(THIRD_USER_ID, SECOND_USER_PASSWORD));
-    assertEquals(THIRD_USER_ID, user.getMail());
+    UserResponse userResponse = userController.save(buildUser(THIRD_USER_ID, SECOND_USER_PASSWORD));
+    assertEquals(THIRD_USER_ID, userResponse.getMail());
   }
 
   @Test
@@ -63,10 +65,10 @@ public class UserIntegrationTest {
   @Sql("classpath:integration/user.sql")
   void deleteTest() {
     userController.delete(buildUser(FIRST_USER_ID, FIRST_USER_PASSWORD));
-    List<User> userList = userController.getAll(FIRST_ADMIN_ID);
-    assertEquals(1, userList.size());
-    User leftUser = userList.get(0);
-    assertEquals(SECOND_USER_ID, leftUser.getMail());
+    List<UserResponse> userResponseList = userController.getAll(FIRST_ADMIN_ID);
+    assertEquals(1, userResponseList.size());
+    UserResponse leftUserResponse = userResponseList.get(0);
+    assertEquals(SECOND_USER_ID, leftUserResponse.getMail());
   }
 
   public static User buildUser(String userId, String userPassword) {
