6aacc17be052721aab54858891917d1af87f141c
Bruno <brunoromano@hotmail.it>
2023-02-11 11:26:23 +0100

    integration test data

---
+++ MusicStore/src/test/resources/integration/user.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE admin;
+INSERT INTO admin (mail,name,surname,phone_number) VALUES ("mail1@test","name1","surname1","111-111-111");
+TRUNCATE TABLE user;
+INSERT INTO user (mail,password,img_url) VALUES ("usermail1@test","password1","img_url1");
+INSERT INTO user (mail,password,img_url) VALUES ("usermail2@test","password2","img_url2");
