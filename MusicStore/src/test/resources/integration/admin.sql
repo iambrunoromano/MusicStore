9231790a273b3c3dc0706d96d8a994d7aff85996
Bruno <brunoromano@hotmail.it>
2023-02-13 07:16:02 +0100

    user data to allow new isAdmin to work properly

--- MusicStore/src/test/resources/integration/admin.sql
+++ MusicStore/src/test/resources/integration/admin.sql
@@ -1,3 +1,6 @@
 TRUNCATE TABLE admin;
 INSERT INTO admin (mail,name,surname,phone_number) VALUES ("mail1@test","name1","surname1","111-111-111");
 INSERT INTO admin (mail,name,surname,phone_number) VALUES ("mail2@test","name2","surname2","222-222-222");
+TRUNCATE TABLE user;
+INSERT INTO user (mail,password,img_url) VALUES ("mail1@test","password1","img_url1");
+INSERT INTO user (mail,password,img_url) VALUES ("mail2@test","password2","img_url2");
