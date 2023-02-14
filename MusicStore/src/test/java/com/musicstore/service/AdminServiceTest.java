62f2592d3dd7a8ddb098a821dee01f45da28521f
Bruno <brunoromano@hotmail.it>
2023-02-14 07:02:07 +0100

    adapting to test utilityand reducing classes cross-dependencies

--- MusicStore/src/test/java/com/musicstore/service/AdminServiceTest.java
+++ MusicStore/src/test/java/com/musicstore/service/AdminServiceTest.java
@@ -1,5 +1,6 @@
 package com.musicstore.service;
 
+import com.musicstore.TestUtility;
 import com.musicstore.constant.ReasonsConstant;
 import com.musicstore.entity.Admin;
 import com.musicstore.entity.User;
@@ -15,14 +16,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class AdminServiceTest {
-
-  private static final String DELETE_ID = "delete-id";
-  private static final String NAME = "name";
-  private static final String SURNAME = "surname";
-  private static final String MAIL = "mail";
-  private static final String PHONE_NUMBER = "phone-number";
-
+public class AdminServiceTest extends TestUtility {
   private AdminRepository adminRepository = Mockito.mock(AdminRepository.class);
   private UserService userService = Mockito.mock(UserService.class);
   private AdminService adminService;
@@ -40,7 +34,8 @@ public class AdminServiceTest {
             () -> {
               adminService.delete(DELETE_ID);
             });
-    assertNotAdminException(actualException);
+    assertReasonException(
+        actualException, HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_ADMIN);
   }
 
   @Test
@@ -52,7 +47,9 @@ public class AdminServiceTest {
             () -> {
               adminService.isAdmin(User.builder().mail(MAIL).build());
             });
-    UserServiceTest.assertNotAuthenticException(actualException);
+    assertReasonException(
+        actualException, HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_AUTHENTIC);
+    ;
   }
 
   @Test
@@ -65,14 +62,15 @@ public class AdminServiceTest {
             () -> {
               adminService.isAdmin(User.builder().mail(MAIL).build());
             });
-    assertNotAdminException(actualException);
+    assertReasonException(
+        actualException, HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_ADMIN);
   }
 
   @Test
   void isAdminAuthorizedTest() {
     mockAuthenticUser();
     BDDMockito.given(adminRepository.findById(Mockito.anyString()))
-        .willReturn(getOptionalAdminBean());
+        .willReturn(buildOptionalAdmin());
     Admin actualAdmin = adminService.isAdmin(User.builder().mail(MAIL).build());
     assertEquals(buildAdmin(), actualAdmin);
   }
@@ -90,21 +88,5 @@ public class AdminServiceTest {
         .willThrow(
             new ResponseStatusException(
                 HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_AUTHENTIC));
-  }
-
-  public static void assertNotAdminException(ResponseStatusException actualException) {
-    ResponseStatusException expectedException =
-        new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, ReasonsConstant.NOT_ADMIN);
-    assertEquals(expectedException.getReason(), actualException.getReason());
-    assertEquals(expectedException.getStatus(), actualException.getStatus());
-  }
-
-  private Optional<Admin> getOptionalAdminBean() {
-    Admin admin = buildAdmin();
-    return Optional.of(admin);
-  }
-
-  public static Admin buildAdmin() {
-    return Admin.builder().mail(MAIL).name(NAME).surname(SURNAME).phoneNumber(PHONE_NUMBER).build();
   }
 }
