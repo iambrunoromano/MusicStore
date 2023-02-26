375eb705d3ad23cddbc95bb89cc54bec21df5c19
Bruno <brunoromano@hotmail.it>
2023-02-26 20:54:01 +0100

    calls refactor

--- musicstoreapp/src/app/services/customer.service.ts
+++ musicstoreapp/src/app/services/customer.service.ts
@@ -4,44 +4,42 @@ import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
 import { Customer } from '../interfaces/customer';
-import { Body } from '../interfaces/body';
-import { WebUser } from '../interfaces/webuser';
+import { User } from '../interfaces/user';
 
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn : 'root'
+  providedIn: 'root'
 })
-export class CustomerService{
+export class CustomerService {
   private root_url = environment.apiBaseUrl;
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type' : 'application/json'
+      'Content-Type': 'application/json'
     })
   }
 
-  private addressAPI: string = "customer";
+  private addressAPI: string = "customers";
 
-  constructor(private http : HttpClient){}
+  constructor(private http: HttpClient) { }
 
-  public getAll(wu: WebUser): Observable<Customer[]>{
-    return this.http.post<Customer[]>(this.root_url + this.addressAPI + '/all',wu);
+  public getAll(user: User): Observable<Customer[]> {
+    this.httpOptions.headers.append('user', JSON.stringify(user));
+    return this.http.get<Customer[]>(this.root_url + this.addressAPI + '/all', this.httpOptions);
   }
 
-    public getById(body: Body): Observable<Customer>{
-      return this.http.post<Customer>(this.root_url + this.addressAPI + '/' + body.topost.mail,body.authorized);
-    }
+  public getById(user: User): Observable<Customer> {
+    this.httpOptions.headers.append('user', JSON.stringify(user));
+    return this.http.get<Customer>(this.root_url + this.addressAPI, this.httpOptions);
+  }
 
-    public create(customer: Customer): Observable<Customer>{
-      return this.http.post<Customer>(this.root_url + this.addressAPI,customer);
-    }
+  public create(user: User, customer: Customer): Observable<Customer> {
+    this.httpOptions.headers.append('user', JSON.stringify(user));
+    return this.http.post<Customer>(this.root_url + this.addressAPI, customer, this.httpOptions);
+  }
 
-    public update(body: Body): Observable<Customer>{
-      return this.http.put<Customer>(this.root_url + this.addressAPI + '/' + body.topost.mail,body);
-    }
-
-    public delete(body: Body): Observable<void>{
-      const options = {body: body.authorized};
-      return this.http.delete<void>(this.root_url + this.addressAPI + '/' + body.topost.mail,options);
-    }
+  public delete(user: User, customerId: number): Observable<void> {
+    this.httpOptions.headers.append('user', JSON.stringify(user));
+    return this.http.delete<void>(this.root_url + this.addressAPI + '/' + customerId, this.httpOptions);
+  }
 }
