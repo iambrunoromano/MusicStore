e4c5900186717bb5c38f1b8b9c0f8884c59a0a0b
Bruno <brunoromano@hotmail.it>
2023-02-26 20:20:18 +0100

    calls refactor

--- musicstoreapp/src/app/services/cart.service.ts
+++ musicstoreapp/src/app/services/cart.service.ts
@@ -3,50 +3,39 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 
-import { Body } from '../interfaces/body';
-import { WebUser } from '../interfaces/webuser';
+import { User } from '../interfaces/user';
 import { Cart } from '../interfaces/cart';
-import { Product } from '../interfaces/product';
+import { CartRequest } from '../interfaces/cartrequest';
 
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn : 'root'
+  providedIn: 'root'
 })
-export class CartService{
+export class CartService {
   private root_url = environment.apiBaseUrl;
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type' : 'application/json'
+      'Content-Type': 'application/json'
     })
   }
 
-  private addressAPI: string = "cart";
+  private addressAPI: string = "carts";
 
-  constructor(private http : HttpClient){}
+  constructor(private http: HttpClient) { }
 
-  public ProductsByCart(wu: WebUser): Observable<Product[]>{
-    return this.http.post<Product[]>(this.root_url + this.addressAPI + '/' + wu.mail + '/products',wu);
+  public getCart(user: User): Observable<Cart[]> {
+    this.httpOptions.headers.append('user', JSON.stringify(user));
+    return this.http.get<Cart[]>(this.root_url + this.addressAPI, this.httpOptions);
   }
 
-  public getAll(wu: WebUser): Observable<Cart[]>{
-    return this.http.post<Cart[]>(this.root_url + this.addressAPI + '/all',wu);
+  public save(user: User, cartRequest: CartRequest): Observable<void> {
+    this.httpOptions.headers.append('user', JSON.stringify(user));
+    return this.http.post<void>(this.root_url + this.addressAPI, cartRequest, this.httpOptions);
   }
 
-  public getById(body: Body): Observable<Cart>{
-    return this.http.post<Cart>(this.root_url + this.addressAPI + '/' + body.topost.id,body.authorized);
-  }
-
-  public create(body: Body): Observable<Cart>{
-    return this.http.post<Cart>(this.root_url + this.addressAPI,body);
-  }
-
-  public update(body: Body): Observable<Cart>{
-    return this.http.put<Cart>(this.root_url + this.addressAPI + '/' + body.topost.id,body);
-  }
-
-  public delete(body: Body): Observable<void>{
-    const options = {body: body.authorized};
-    return this.http.delete<void>(this.root_url + this.addressAPI + '/' + body.topost.id,options);
+  public delete(user: User): Observable<void> {
+    this.httpOptions.headers.append('user', JSON.stringify(user));
+    return this.http.delete<void>(this.root_url + this.addressAPI, this.httpOptions);
   }
 }
