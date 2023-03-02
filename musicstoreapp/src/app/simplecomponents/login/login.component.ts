6791ac2bcb19b36b0541250cb90c5a09ae9f7f9c
Bruno <brunoromano@hotmail.it>
2023-03-02 07:25:42 +0100

    adapted to new user service method

--- musicstoreapp/src/app/simplecomponents/login/login.component.ts
+++ musicstoreapp/src/app/simplecomponents/login/login.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 
 import { HttpErrorResponse } from '@angular/common/http';
 
-import { LogStatus } from '../../interfaces/logstatus';
-import { WebUser } from '../../interfaces/webuser';
-import { Body } from '../../interfaces/body';
-import { WebUserService } from '../../services/webuser.service';
+import { User } from '../../interfaces/user';
+import { UserResponse } from '../../interfaces/response/userresponse';
+import { UserService } from '../../services/user.service';
 import { DataService } from '../../services/data.service';
 
 @Injectable({
-  providedIn : 'root'
+  providedIn: 'root'
 })
 @Component({
   selector: 'app-login',
@@ -20,28 +20,28 @@ import { DataService } from '../../services/data.service';
 })
 export class LoginComponent implements OnInit {
 
-  public logstatus = <LogStatus>{ };
-
-  constructor(private webuserService : WebUserService,
-              private dataService : DataService,
-              private router : Router) { }
+  constructor(private userService: UserService,
+    private dataService: DataService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.dataService.setLogStatus(false);
-    this.dataService.setUserData('','');
+    this.dataService.setUserData('', '');
   }
 
-  public login(): void{
-    let wu : WebUser = {mail:(<HTMLInputElement>document.getElementById("username"))?.value,
-                        password:(<HTMLInputElement>document.getElementById("password"))?.value};
-    this.webuserService.login(wu).subscribe(
-      (response: LogStatus) => {
-        this.logstatus = response;
-        this.dataService.setUserData(wu.mail, wu.password);
+  public login(): void {
+    let user: User = {
+      mail: (<HTMLInputElement>document.getElementById("username"))?.value,
+      password: (<HTMLInputElement>document.getElementById("password"))?.value,
+      imgurl: ''
+    };
+    this.userService.getById(user).subscribe(
+      (response: UserResponse) => {
+        this.dataService.setUserData(response.mail, user.password);
         this.dataService.setLogStatus(true);
         this.router.navigate(['/']);
       },
-      (error : HttpErrorResponse) => {
+      (error: HttpErrorResponse) => {
         alert(error.message);
         this.router.navigate(['/login']);
       }
