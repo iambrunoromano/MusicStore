79182bdecbd196dfbcbe17589f3bb93c437749aa
Bruno <brunoromano@hotmail.it>
2023-03-07 07:23:17 +0100

    validated fields

--- MusicStore/src/main/java/com/musicstore/request/UserRequest.java
+++ MusicStore/src/main/java/com/musicstore/request/UserRequest.java
@@ -3,10 +3,14 @@ package com.musicstore.request;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 public class UserRequest {
-  // TODO: add mail & password validation
-  private String mail;
+  @Email @NotBlank private String mail;
+
+  @NotBlank(message = "Password can't be blank")
   private String password;
 }
