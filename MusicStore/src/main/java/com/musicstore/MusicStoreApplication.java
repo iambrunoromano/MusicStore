d32762d16415ae0a42332bd7a627558bf83163fc
Bruno <brunoromano@hotmail.it>
2023-10-11 13:47:48 +0200

    allow all input and output headers

--- MusicStore/src/main/java/com/musicstore/MusicStoreApplication.java
+++ MusicStore/src/main/java/com/musicstore/MusicStoreApplication.java
@@ -22,26 +22,8 @@ public class MusicStoreApplication {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.setAllowCredentials(true);
     corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
-    corsConfiguration.setAllowedHeaders(
-        Arrays.asList(
-            "Origin",
-            "Access-Control-Allow-Origin",
-            "Content-Type",
-            "Accept",
-            "Authorization",
-            "Origin, Accept",
-            "X-Requested-With",
-            "Access-Control-Request-Method",
-            "Access-Control-Request-Headers"));
-    corsConfiguration.setExposedHeaders(
-        Arrays.asList(
-            "Origin",
-            "Content-Type",
-            "Accept",
-            "Authorization",
-            "Access-Control-Allow-Origin",
-            "Access-Control-Allow-Origin",
-            "Access-Control-Allow-Credentials"));
+    corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+    corsConfiguration.setExposedHeaders(Arrays.asList("*"));
     corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource =
         new UrlBasedCorsConfigurationSource();
